5732ae9e861de8d70fcee2f51852a1b93e3c6da0
AAKASH GUPTA <73053750+aakashgupta0001@users.noreply.github.com>
2024-08-25 18:07:21 +0530

    Add files via upload

---
+++ RECTANGLE.c
@@ -0,0 +1,13 @@
+// W.A.P TO FIND THE AREA OF TRIANGLE 
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of Length:");
+    scanf("%d",&a);
+    printf("Enter the value of Breadth:");
+    scanf("%d",&b);
+    c=a*b;
+    printf("Area of Rectangle is:%d",c);
+    return 0;
+}
