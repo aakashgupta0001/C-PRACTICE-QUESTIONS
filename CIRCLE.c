322171aba9ee07ba6809fa34bd88c8d72964cc8f
AAKASH GUPTA <73053750+aakashgupta0001@users.noreply.github.com>
2024-08-25 18:11:32 +0530

    Add files via upload
    W.A.P TO FIND THE AREA OF CIRCLE

---
+++ CIRCLE.c
@@ -0,0 +1,12 @@
+// W.A.P TO FIND THE AREA OF CIRCLE
+#include<stdio.h>
+int main()
+{
+    float r,area;
+    printf("Enter the radius: ");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("The area of circle is: %.2f",area);
+    return 0;
+    
+}
